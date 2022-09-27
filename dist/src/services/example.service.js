9f3c3899aa0148ce10c83e6e93b027d541eda844
wmartzh <wmartzh@gmail.com>
2022-09-27 16:10:48 -0600

    remove dist folder

--- dist/src/services/example.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ExampleService {
-    getMessage() {
-        return "Hello world";
-    }
-}
-exports.default = new ExampleService();
